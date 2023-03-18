5aa8901e335e39dcf36c51347a411641af25a9f8
muneeraalnufaisi <m.alnufaisi@takamol.com.sa>
2023-03-18 12:46:11 +0300

    add postgresql_psycopg2

--- ddata/settings.py
+++ ddata/settings.py
@@ -84,13 +84,21 @@ WSGI_APPLICATION = 'ddata.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
+DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         # 'NAME': BASE_DIR / 'db.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+#     },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '', 
+        'USER': '', 
+        'PASSWORD': '',
+        'HOST': '', 
+        'PORT': '',
+    }
+}
 
 
 # Password validation
